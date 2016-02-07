9cd2af59fcf635e323405df2fc25ac6eb2c8e2d9
Jakub Konka <kubkon@jakubkonka.com>
2016-02-07 15:49:52 +0000

    Implement the meat behind rabin karp algorithm

--- src/rabin_karp.rs
+++ src/rabin_karp.rs
@@ -1,12 +1,12 @@
 use ::core::StringSearch;
 
 pub struct StringMatcher {
-    alphabet_size: usize,
-    modulus_prime: usize,
+    alphabet_size: u32,
+    modulus_prime: u32,
 }
 
 impl StringMatcher {
-    pub fn new(alphabet_size: usize, modulus_prime: usize) -> StringMatcher {
+    pub fn new(alphabet_size: u32, modulus_prime: u32) -> StringMatcher {
         StringMatcher {
             alphabet_size: alphabet_size,
             modulus_prime: modulus_prime,
@@ -16,7 +16,29 @@ impl StringMatcher {
 
 impl StringSearch for StringMatcher {
     fn _search(&self, text: &str, pattern: &str) -> Vec<usize> {
-        Vec::new()
+        let mut indices = Vec::new();
+        let m = text.len();
+        let n = pattern.len();
+        let h = self.alphabet_size.pow(n as u32 - 1) % self.modulus_prime;
+        // pre-processing
+        let text_b: Vec<u32> = text.as_bytes().iter().map(|&x| x as u32).collect();
+        let pattern_b: Vec<u32> = pattern.as_bytes().iter().map(|&x| x as u32).collect();
+        let (p, mut t) = pattern_b.iter()
+                                  .zip(text_b.iter())
+                                  .fold((0,0), |(p, t), (&x, &y)| (
+                                      (p * self.alphabet_size + x) % self.modulus_prime,
+                                      (t * self.alphabet_size + y) % self.modulus_prime,
+                                  ));
+        // matching
+        for i in 0 .. m - n {
+            if p == t {
+                if &text[i .. n + i] == pattern {
+                    indices.push(i);
+                }
+            }
+            t = (self.alphabet_size * (t - h * text_b[i]) + text_b[i + n]) % self.modulus_prime
+        }
+        indices
     }
 }
 
@@ -41,5 +63,7 @@ mod tests {
         assert_eq!(vec![3],   matcher.search("abcabaabcabac", "abaa"));
         assert_eq!(vec![0,6], matcher.search("abcabaabcabac", "abc"));
         assert_eq!(vec![3,9], matcher.search("abcabaabcabac", "aba"));
+        assert_eq!(vec![0,3,10], matcher.search("abcabaacbaabc", "ab"));
+        assert_eq!(vec![0,10], matcher.search("abcabaacbaabc", "abc"));
     }
 }
