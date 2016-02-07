19161c3504f97404a92805e06b0a7bd2f4fffb5e
Jakub Konka <kubkon@jakubkonka.com>
2016-02-07 21:49:10 +0000

    Fix overflow issues with rabin karp algorithm

--- src/rabin_karp.rs
+++ src/rabin_karp.rs
@@ -1,16 +1,14 @@
 use ::core::StringSearch;
 
+static ALPHABET: isize = 256;
+
 pub struct StringMatcher {
-    alphabet_size: u32,
-    modulus_prime: u32,
+    prime: isize,
 }
 
 impl StringMatcher {
-    pub fn new(alphabet_size: u32, modulus_prime: u32) -> StringMatcher {
-        StringMatcher {
-            alphabet_size: alphabet_size,
-            modulus_prime: modulus_prime,
-        }
+    pub fn new(prime: isize) -> StringMatcher {
+        StringMatcher { prime: prime }
     }
 }
 
@@ -19,24 +17,27 @@ impl StringSearch for StringMatcher {
         let mut indices = Vec::new();
         let m = text.len();
         let n = pattern.len();
-        let h = self.alphabet_size.pow(n as u32 - 1) % self.modulus_prime;
+        let h = (0 .. n - 1).fold(1, |acc, _| (acc * ALPHABET) % self.prime);
         // pre-processing
-        let text_b: Vec<u32> = text.as_bytes().iter().map(|&x| x as u32).collect();
-        let pattern_b: Vec<u32> = pattern.as_bytes().iter().map(|&x| x as u32).collect();
+        let text_b: Vec<isize> = text.as_bytes().iter().map(|&x| x as isize).collect();
+        let pattern_b: Vec<isize> = pattern.as_bytes().iter().map(|&x| x as isize).collect();
         let (p, mut t) = pattern_b.iter()
                                   .zip(text_b.iter())
                                   .fold((0,0), |(p, t), (&x, &y)| (
-                                      (p * self.alphabet_size + x) % self.modulus_prime,
-                                      (t * self.alphabet_size + y) % self.modulus_prime,
+                                      (p * ALPHABET + x) % self.prime,
+                                      (t * ALPHABET + y) % self.prime,
                                   ));
         // matching
-        for i in 0 .. m - n {
+        for i in 0 .. m - n + 1 {
             if p == t {
                 if &text[i .. n + i] == pattern {
                     indices.push(i);
                 }
             }
-            t = (self.alphabet_size * (t - h * text_b[i]) + text_b[i + n]) % self.modulus_prime
+            if i < m - n {
+                t = (ALPHABET * (t - text_b[i] * h) + text_b[i + n]) % self.prime;
+                t = if t < 0 { t + self.prime } else { t };
+            }
         }
         indices
     }
@@ -49,7 +50,7 @@ mod tests {
 
     #[test]
     fn invalid_pattern() {
-        let matcher = StringMatcher::new(26, 13);
+        let matcher = StringMatcher::new(101);
         let expected: Vec<usize> = Vec::new();
         assert_eq!(expected, matcher.search("abc", "aaaa"));
         assert_eq!(expected, matcher.search("abc", ""));
@@ -59,7 +60,7 @@ mod tests {
 
     #[test]
     fn valid_pattern() {
-        let matcher = StringMatcher::new(26, 13);
+        let matcher = StringMatcher::new(101);
         assert_eq!(vec![3],   matcher.search("abcabaabcabac", "abaa"));
         assert_eq!(vec![0,6], matcher.search("abcabaabcabac", "abc"));
         assert_eq!(vec![3,9], matcher.search("abcabaabcabac", "aba"));
